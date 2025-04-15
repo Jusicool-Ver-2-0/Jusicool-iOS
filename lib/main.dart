fc7157b2585a149a2b0d030df219a1df6bfc7309
김담율 <s24001@gsm.hs.kr>
2025-04-15 20:08:37 +0900

    ✨ 스플래시 스크린 기능 구현 & 프로젝트 기초 새팅
    스플래시 스크린 기능 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:jusicool_ios/screens/splash_screen.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Jusicool',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('메인 화면')),
+      body: const Center(
+        child: Text('메인 화면입니다.', style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
