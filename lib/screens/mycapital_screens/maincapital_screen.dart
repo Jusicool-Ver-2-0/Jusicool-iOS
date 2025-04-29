80255c2df1e8c25dd3fdb5353ba77da89f56df15
김담율 <s24001@gsm.hs.kr>
2025-04-29 18:08:12 +0900

    ⚡️ :: 주문내역 상세페이지 퍼블리싱
    주문내역 상세페이지 퍼블리싱, maincapital에서의 경로 설정

--- lib/screens/mycapital_screens/maincapital_screen.dart
+++ lib/screens/mycapital_screens/maincapital_screen.dart
@@ -4,6 +4,7 @@ import 'package:intl/intl.dart';
 import 'package:jusicool_design_system/src/core/theme/colors/color_palette.dart';
 import 'package:jusicool_design_system/src/core/theme/texts/typography.dart';
 import 'package:jusicool_ios/screens/login_screen.dart';
+import 'package:jusicool_ios/screens/mycapital_screens/order_detail.dart';
 
 class StockCard extends StatelessWidget {
   final String imagePath;
@@ -546,7 +547,7 @@ class MainCapitalScreen extends StatelessWidget {
                                                   MaterialPageRoute(
                                                     builder:
                                                         (context) =>
-                                                            LoginScreen(),
+                                                            OrderDetailScreen(),
                                                   ),
                                                 );
                                               },
