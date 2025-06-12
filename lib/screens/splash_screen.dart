0fdeb885b26ba8e15b7fa62aff1b5516aa17c508
bluemoon983 <msdrobot@naver.com>
2025-06-12 01:44:27 -0700

    ♻️ Splashscreen 수정

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jusicool_design_system/src/core/theme/colors/color_palette.dart';
 import 'package:jusicool_design_system/src/core/theme/texts/typography.dart';
-import 'package:jusicool_ios/screens/community_screens/community_post_list_screen.dart';
 import 'package:jusicool_ios/screens/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -20,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
     Timer(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => CommunityPostListScreen()),
+        MaterialPageRoute(builder: (context) => LoginScreen()),
       );
     });
   }
