5845add4084658736560eceb88b0eb3ff8eea697
parkyuhyeon <pyh1111802@naver.com>
2025-06-30 20:04:56 +0900

    :sparkles: :: Adds user authentication DTOs
    Defines data transfer objects (DTOs) for user sign-in and sign-up requests. These DTOs facilitate communication between the client and server for user authentication purposes.
    The DTOs are generated using Freezed for immutability and JSON serialization.

---
+++ lib/data/user/dto/remote/request/sign_in_request_dto.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'sign_in_request_dto.g.dart';
+
+part 'sign_in_request_dto.freezed.dart';
+
+@freezed
+abstract class SignInRequestDto with _$SignInRequestDto {
+  factory SignInRequestDto({required String email, required String password}) =
+      _SignInRequestDto;
+
+  factory SignInRequestDto.fromJson(Map<String, dynamic> json) =>
+      _$SignInRequestDtoFromJson(json);
+}
---
+++ lib/data/user/dto/remote/request/sign_up_request_dto.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'sign_up_request_dto.g.dart';
+
+part 'sign_up_request_dto.freezed.dart';
+
+@freezed
+abstract class SignUpRequestDto with _$SignUpRequestDto {
+  factory SignUpRequestDto({
+    required String email,
+    required String password,
+    required String name,
+  }) = _SignUpRequestDto;
+
+  factory SignUpRequestDto.fromJson(Map<String, dynamic> json) =>
+      _$SignUpRequestDtoFromJson(json);
+}
