e99b572c55eabff96eae059e37f1b40fb53eed82
parkyuhyeon <pyh1111802@naver.com>
2025-06-30 20:03:58 +0900

    :sparkles: :: Adds user API for authentication
    Defines the user API using Retrofit for handling sign-in, sign-up, email sending, and email verification functionalities.
    This API will serve as the interface for interacting with the user authentication backend.

---
+++ lib/data/user/api/user_api.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/error_logger.dart';
+import 'package:retrofit/http.dart';
+import '../dto/remote/request/sign_in_request_dto.dart';
+import '../dto/remote/request/sign_up_request_dto.dart';
+
+part 'user_api.g.dart';
+
+@RestApi()
+abstract class UserApi {
+  factory UserApi(Dio dio, {String baseUrl}) = _UserApi;
+
+  @POST('/user/signin')
+  Future<void> signIn(@Body() SignInRequestDto body);
+
+  @POST('/user/signup')
+  Future<void> signUp(@Body() SignUpRequestDto body);
+
+  @POST('/user/email/send')
+  Future<void> sendEmail();
+
+  @POST('/user/email/verify')
+  Future<void> verifyEmail();
+}
