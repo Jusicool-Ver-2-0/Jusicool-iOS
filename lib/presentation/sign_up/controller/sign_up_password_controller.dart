59badd6edb8d160cfd061d3f900c81b749d40421
parkyuhyeon <pyh1111802@naver.com>
2025-07-08 16:03:28 +0900

    :sparkles: :: Adds signup password controller
    Implements the signup password controller using Riverpod.
    
    This controller manages the state and logic for the password and confirm password fields in the signup process.
    It includes validation for password strength and matching confirmation password.
    It also handles navigation to the next step in the signup flow.

---
+++ lib/presentation/sign_up/controller/sign_up_password_controller.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:jusicool_ios/presentation/sign_up/state/sign_up_password_state.dart';
+
+import '../../../core/config/router/router.dart';
+
+final signupPasswordControllerProvider =
+    StateNotifierProvider<SignupPasswordController, SignUpPasswordState>(
+      (ref) => SignupPasswordController(),
+    );
+
+class SignupPasswordController extends StateNotifier<SignUpPasswordState> {
+  SignupPasswordController() : super(SignUpPasswordState()) {
+    _passwordController.addListener(_onPasswordChanged);
+    _confirmPasswordController.addListener(_onConfirmPasswordChanged);
+  }
+
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  TextEditingController get passwordController => _passwordController;
+
+  TextEditingController get confirmPasswordController =>
+      _confirmPasswordController;
+
+  void _onPasswordChanged() {
+    final password = _passwordController.text.trim();
+    state = state.copyWith(password: password, isPasswordValid: true);
+  }
+
+  void _onConfirmPasswordChanged() {
+    _updateFormState();
+    checkPasswordsMatch();
+  }
+
+  void _updateFormState() {
+    state = state.copyWith(
+      isFormFilled:
+          _passwordController.text.isNotEmpty &&
+          _confirmPasswordController.text.isNotEmpty,
+    );
+  }
+
+  void validatePassword(String password) {
+    state = state.copyWith(isPasswordValid: isValidPassword(password));
+  }
+
+  void checkPasswordsMatch() {
+    state = state.copyWith(
+      isPasswordMatched:
+          _passwordController.text == _confirmPasswordController.text,
+    );
+  }
+
+  bool isValidPassword(String password) {
+    if (password.length < 8 || password.length > 13) return false;
+
+    final hasLetter = RegExp(r'[A-Za-z]').hasMatch(password);
+    final hasNumber = RegExp(r'\d').hasMatch(password);
+    final hasSpecial = RegExp(r'[@$!%*?&]').hasMatch(password);
+
+    final count = [hasLetter, hasNumber, hasSpecial].where((e) => e).length;
+    return count >= 2;
+  }
+
+  void onNextButtonPressed(BuildContext context) {
+    context.push(RoutePaths.findSchool);
+  }
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+}
