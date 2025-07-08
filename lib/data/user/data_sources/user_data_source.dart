7dd094d7889571266fdc44aa64aa3226ebb2176e
parkyuhyeon <pyh1111802@naver.com>
2025-07-08 15:59:41 +0900

    :sparkles: :: Adds user data source implementation
    Implements the user data source and its implementation.
    It introduces methods for user authentication,
    email verification, and searching schools.
    It also creates a remote data source to handle the
    requests and responses from the APIs.

--- lib/data/user/data_sources/user_data_source.dart
+++ lib/data/user/data_sources/user_data_source.dart
@@ -1,15 +1,20 @@
-import 'package:dio/dio.dart';
-
+import 'package:jusicool_ios/data/user/dto/remote/request/sign_up_search_school_request_dto.dart';
+import 'package:jusicool_ios/data/user/dto/remote/request/sign_up_send_email_request_dto.dart';
+import 'package:jusicool_ios/data/user/dto/remote/request/sign_up_verify_email_request_dto.dart';
+import 'package:jusicool_ios/data/user/dto/remote/response/sign_up_search_school_response_dto.dart';
 import '../dto/remote/request/sign_in_request_dto.dart';
 import '../dto/remote/request/sign_up_request_dto.dart';
 
 abstract class UserDataSource {
-
   Future<void> signIn(SignInRequestDto body);
 
   Future<void> signUp(SignUpRequestDto body);
 
-  Future<void> sendEmail();
+  Future<void> sendEmail(SignUpSendEmailRequestDto body);
 
-  Future<void> verifyEmail();
+  Future<void> verifyEmail(SignUpVerifyEmailRequestDto body);
+
+  Future<SignUpSearchSchoolResponseDto> searchSchools(
+    SignUpSearchSchoolRequestDto schoolName,
+  );
 }
--- lib/data/user/data_sources/user_data_source_impl.dart
+++ lib/data/user/data_sources/user_data_source_impl.dart
@@ -1,12 +1,18 @@
-import 'package:jusicool_ios/data/user/api/user_api.dart';
+import 'package:jusicool_ios/data/user/dto/remote/request/sign_up_search_school_request_dto.dart';
+import 'package:jusicool_ios/data/user/dto/remote/request/sign_up_send_email_request_dto.dart';
+import 'package:jusicool_ios/data/user/dto/remote/request/sign_up_verify_email_request_dto.dart';
+import 'package:jusicool_ios/data/user/service/neis_api.dart';
+import 'package:jusicool_ios/data/user/service/user_api.dart';
 import 'package:jusicool_ios/data/user/data_sources/user_data_source.dart';
 import 'package:jusicool_ios/data/user/dto/remote/request/sign_in_request_dto.dart';
 import 'package:jusicool_ios/data/user/dto/remote/request/sign_up_request_dto.dart';
+import '../dto/remote/response/sign_up_search_school_response_dto.dart';
 
 class UserDataSourceImpl extends UserDataSource {
   final UserApi _userApi;
+  final NeisApi _neisClient;
 
-  UserDataSourceImpl(this._userApi);
+  UserDataSourceImpl(this._userApi, this._neisClient);
 
   @override
   Future<void> signIn(SignInRequestDto body) async {
@@ -19,12 +25,19 @@ class UserDataSourceImpl extends UserDataSource {
   }
 
   @override
-  Future<void> sendEmail() async {
-    return await _userApi.sendEmail();
+  Future<void> sendEmail(SignUpSendEmailRequestDto body) async {
+    return await _userApi.sendEmail(body);
   }
 
   @override
-  Future<void> verifyEmail() async {
-    return await _userApi.verifyEmail();
+  Future<void> verifyEmail(SignUpVerifyEmailRequestDto body) async {
+    return await _userApi.verifyEmail(body);
+  }
+
+  @override
+  Future<SignUpSearchSchoolResponseDto> searchSchools(
+    SignUpSearchSchoolRequestDto school,
+  ) async {
+    return await _neisClient.fetchSchools(school.schoolName);
   }
 }
