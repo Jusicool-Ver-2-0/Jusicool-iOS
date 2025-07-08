8a619bd8263960e94c86266cce230818f1bdd6d4
aiden30015 <loolloo080313@gmail.com>
2025-07-09 08:35:46 +0900

    ✨ :: Exchange usecase class

---
+++ lib/domain/exchange/usecase/exchange_usercase_impl.dart
@@ -0,0 +1,20 @@
+import 'package:jusicool_ios/domain/exchange/entity/exchange_entity.dart';
+import 'package:jusicool_ios/domain/exchange/repositories/exchange_repository.dart';
+import 'package:jusicool_ios/domain/exchange/usecase/exchange_usecase.dart';
+
+class ExchangeUsercaseImpl extends ExchangeUsecase {
+
+  final ExchangeRepository _repository;
+
+  ExchangeUsercaseImpl(this._repository);
+
+  @override
+  Future<void> exchangeKRW(ExchangeEntity entity) async { 
+    await _repository.exchangeKRW(entity);
+  }
+  
+  @override
+  Future<void> exchangeUSD(ExchangeEntity entity) async {
+    await _repository.exchangeUSD(entity);
+  }
+}
