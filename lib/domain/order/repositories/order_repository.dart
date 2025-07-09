c23cc789d9e9b716cf5896e0f75c99fd4d2e7029
aiden30015 <loolloo080313@gmail.com>
2025-07-09 20:45:37 +0900

    ✨ :: Order abstrac repository class

---
+++ lib/domain/order/repositories/order_repository.dart
@@ -0,0 +1,17 @@
+import 'package:jusicool_ios/domain/enum/share/order/reserve_type.dart';
+import 'package:jusicool_ios/domain/order/entities/month_order_entity.dart';
+import 'package:jusicool_ios/domain/order/entities/month_rate_entity.dart';
+import 'package:jusicool_ios/domain/order/entities/my_order_entity.dart';
+import 'package:jusicool_ios/domain/order/entities/reservation_order_entity.dart';
+
+abstract class OrderRepository {
+  Future<MyOrderEntity> fetchMyorders(List<ReserveType> type);
+
+  Future<MonthOrderEntity> fetchMonthOrder();
+
+  Future<MonthRateEntity> fetchMonthRate();
+
+  Future<int> order(double quantity);
+
+  Future<ReservationOrderEntity> fetchReservationOrder();
+}
