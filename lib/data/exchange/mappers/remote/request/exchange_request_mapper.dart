883061afb1e95e9b2920d9bddc37a35bf2ed1460
aiden30015 <loolloo080313@gmail.com>
2025-07-09 09:56:59 +0900

    ✨ :: Exchange request mapper class

---
+++ lib/data/exchange/mappers/remote/request/exchange_request_mapper.dart
@@ -0,0 +1,9 @@
+import 'package:jusicool_ios/data/exchange/dto/remote/request/exchange_request_dto.dart';
+import 'package:jusicool_ios/domain/exchange/entity/exchange_entity.dart';
+
+class ExchangeRequestMapper {
+  static ExchangeRequestDto toDto(ExchangeEntity entity) => 
+      ExchangeRequestDto(amount: entity.amount);
+  static ExchangeEntity toEntity(ExchangeRequestDto dto) =>
+      ExchangeEntity(amount: dto.amount);
+}
