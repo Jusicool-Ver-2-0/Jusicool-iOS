c63cdfd2699dc94fad98022e7b6dc658cb149827
aiden30015 <loolloo080313@gmail.com>
2025-07-09 20:59:33 +0900

    ✨ :: Exchange data source impl class

--- lib/data/exchange/data_sources/exchange_data_source_impl.dart
+++ lib/data/exchange/data_sources/exchange_data_source_impl.dart
@@ -1,20 +1,21 @@
 import 'package:jusicool_ios/data/exchange/data_sources/exchange_data_source.dart';
 import 'package:jusicool_ios/data/exchange/dto/remote/request/exchange_request_dto.dart';
+import 'package:jusicool_ios/data/exchange/serivice/exchange_api.dart';
 
 class ExchangeDataSourceImpl extends ExchangeDataSource {
 
-  
+  final ExchangeApi _exchangeApi;
+
+  ExchangeDataSourceImpl(this._exchangeApi);
 
   @override
-  Future<void> exchangeKRW(ExchangeRequestDto body) {
-    // TODO: implement exchangeKRW
-    throw UnimplementedError();
+  Future<void> exchangeKRW(ExchangeRequestDto body) async {
+    return await _exchangeApi.exchangeKRW(body);
   }
 
   @override
-  Future<void> exchangeUSD(ExchangeRequestDto body) {
-    // TODO: implement exchangeUSD
-    throw UnimplementedError();
+  Future<void> exchangeUSD(ExchangeRequestDto body) async {
+    return await _exchangeApi.exchangeUSD(body);
   }
   
 }
