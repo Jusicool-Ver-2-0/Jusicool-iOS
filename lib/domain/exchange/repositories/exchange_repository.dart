d5447de961fd1357ab46b69e0d4ee4132b4ac8df
aiden30015 <loolloo080313@gmail.com>
2025-07-09 20:43:42 +0900

    ♻️ :: 폴더 이름 변경

--- lib/domain/exchange/repositories/exchange_repository.dart
+++ lib/domain/exchange/repositories/exchange_repository.dart
@@ -1,4 +1,4 @@
-import 'package:jusicool_ios/domain/exchange/entity/exchange_entity.dart';
+import 'package:jusicool_ios/domain/exchange/entities/exchange_entity.dart';
 
 abstract class ExchangeRepository {
   Future<void> exchangeKRW(ExchangeEntity exchange);
