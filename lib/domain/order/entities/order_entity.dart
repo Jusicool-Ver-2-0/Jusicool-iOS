6988eea537e21591a658d03228ebd6ea795505e7
aiden30015 <loolloo080313@gmail.com>
2025-07-09 23:32:55 +0900

    ✨ :: Order Entity class

---
+++ lib/domain/order/entities/order_entity.dart
@@ -0,0 +1,10 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'order_entity.freezed.dart';
+
+@freezed
+abstract class OrderEntity with _$OrderEntity {
+  factory OrderEntity({
+    required int price,
+  }) = _OrderEntity;
+} 
