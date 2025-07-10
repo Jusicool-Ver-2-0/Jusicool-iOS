e198d2c9a0f1aaf108a4a10018431b8f625b6ac0
aiden30015 <loolloo080313@gmail.com>
2025-07-10 10:40:52 +0900

    ♻️ :: 명세서에 맞게 수정

--- lib/domain/order/usecase/order_usecase.dart
+++ lib/domain/order/usecase/order_usecase.dart
@@ -2,6 +2,7 @@ import 'package:jusicool_ios/domain/enum/share/order/status_type.dart';
 import 'package:jusicool_ios/domain/order/entities/month_order_entity.dart';
 import 'package:jusicool_ios/domain/order/entities/month_rate_entity.dart';
 import 'package:jusicool_ios/domain/order/entities/my_order_entity.dart';
+import 'package:jusicool_ios/domain/order/entities/order_request_entity.dart';
 import 'package:jusicool_ios/domain/order/entities/order_response_entity.dart';
 import 'package:jusicool_ios/domain/order/entities/reserve_order_entity.dart';
 
@@ -12,7 +13,11 @@ abstract class OrderUsecase {
 
   Future<MonthRateEntity> fetchMonthRate();
 
-  Future<OrderEntity> order(double quantity, String merketCode);
+  Future<OrderResponseEntity> buyOrder(OrderRequestEntity quantity, String marketCode);
 
-  Future<ReserveOrderEntity> fetchReservationOrder();
+  Future<OrderResponseEntity> sellOrder(OrderRequestEntity quantity, String marketCode);
+
+  Future<void> reserveBuyOrder(ReserveOrderEntity body, String marketCode);
+
+  Future<void> reserveSellOrder(ReserveOrderEntity body, String marketCode);
 }
