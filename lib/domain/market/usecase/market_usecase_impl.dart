ce971dd1aaad91133ef2e0786a05853b3ee9228f
aiden30015 <loolloo080313@gmail.com>
2025-07-10 15:55:29 +0900

    ✨ :: Market usecase impl class

---
+++ lib/domain/market/usecase/market_usecase_impl.dart
@@ -0,0 +1,18 @@
+import 'package:jusicool_ios/domain/market/entities/market_entity.dart';
+import 'package:jusicool_ios/domain/market/repositories/market_repository.dart';
+import 'package:jusicool_ios/domain/market/usecase/market_usecase.dart';
+
+class MarketUsecaseImpl extends MarketUsecase {
+  final MarketRepository _repository;
+  MarketUsecaseImpl(this._repository);
+
+  @override
+  Future<List<MarketEntity>> fetchMarkets(String type) async {
+    return await _repository.fetchMarkets(type);
+  }
+
+  @override
+  Future<List<MarketEntity>> searchMarkets(String query) async {
+    return await _repository.searchMarkets(query);
+  }
+} 
