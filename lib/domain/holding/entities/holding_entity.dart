7bf2f1ff62f41d1b1fb2c1a0b6a4c7bc96b77075
aiden30015 <loolloo080313@gmail.com>
2025-07-10 11:20:56 +0900

    🔥 :: Jsonkey 어노테이션 삭제

--- lib/domain/holding/entities/holding_entity.dart
+++ lib/domain/holding/entities/holding_entity.dart
@@ -16,8 +16,8 @@ abstract class HoldingEntity with _$HoldingEntity {
 abstract class MarketEntity with _$MarketEntity {
   const factory MarketEntity({
     required int id,
-    @JsonKey(name: 'korean_name') required String koreanName,
-    @JsonKey(name: 'english_name') required String englishName,
+    required String koreanName,
+    required String englishName,
     required String market,
     required String type,
   }) = _MarketEntity;
