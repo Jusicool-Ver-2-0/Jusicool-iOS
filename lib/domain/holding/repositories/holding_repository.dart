0df037f86a0a42c13b44829e98f8664ba18306c2
aiden30015 <loolloo080313@gmail.com>
2025-07-10 11:57:35 +0900

    ♻️ :: 파일명 변경

--- lib/domain/holding/repositories/holding_repository.dart
+++ lib/domain/holding/repositories/holding_repository.dart
@@ -1,5 +1,5 @@
-import 'package:jusicool_ios/domain/holding/entities/holding_entity.dart';
+import 'package:jusicool_ios/domain/holding/entities/my_holding_entity.dart';
 
 abstract class HoldingRepository {
-  Future<HoldingEntity> fetchMyholding();
+  Future<MyHoldingEntity> fetchMyholding();
 }
