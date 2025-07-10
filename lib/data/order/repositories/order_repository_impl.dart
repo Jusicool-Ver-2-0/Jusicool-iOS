5cc58dec486f9af9e3221c385895046e3d770596
aiden30015 <loolloo080313@gmail.com>
2025-07-10 10:34:44 +0900

    ✨ :: Order repository impl class

--- lib/data/order/repositories/order_repository_impl.dart
+++ lib/data/order/repositories/order_repository_impl.dart
@@ -1,5 +1,9 @@
 import 'package:jusicool_ios/data/order/data_sources/order_data_source.dart';
+import 'package:jusicool_ios/data/order/dto/remote/request/order_request_dto.dart';
+import 'package:jusicool_ios/data/order/dto/remote/request/reserve_order_request_dto.dart';
+import 'package:jusicool_ios/data/order/dto/remote/response/order_response_dto.dart';
 import 'package:jusicool_ios/data/order/mapper/remote/request/order_request_mapper.dart';
+import 'package:jusicool_ios/data/order/mapper/remote/request/reserve_order_mapper.dart';
 import 'package:jusicool_ios/data/order/mapper/remote/response/month_order_response_mapper.dart';
 import 'package:jusicool_ios/data/order/mapper/remote/response/month_rate_response_mapper.dart';
 import 'package:jusicool_ios/data/order/mapper/remote/response/my_order_response_mapper.dart';
@@ -13,7 +17,6 @@ import 'package:jusicool_ios/domain/order/entities/reserve_order_entity.dart';
 import 'package:jusicool_ios/domain/order/repositories/order_repository.dart';
 import 'package:jusicool_ios/data/order/dto/remote/request/my_order_query_type_request_dto.dart';
 import 'package:jusicool_ios/data/order/dto/remote/request/my_order_request_dto.dart';
-import 'package:jusicool_ios/data/order/dto/remote/request/order_request_dto.dart';
 import 'package:jusicool_ios/data/order/mapper/remote/response/order_response_mapper.dart';
 
 class OrderRepositoryImpl extends OrderRepository {
@@ -51,21 +54,21 @@ class OrderRepositoryImpl extends OrderRepository {
 
     @override
   Future<OrderResponseEntity> sellOrder(OrderRequestEntity body, String marketCode) async {
-    final requestDto = OrderRequestMapper.toDto(body);
-    final response = await _orderDataSource.buyOrder(requestDto, marketCode);
+    final OrderRequestDto requestDto = OrderRequestMapper.toDto(body);
+    final OrderResponseDto response = await _orderDataSource.buyOrder(requestDto, marketCode);
     return OrderResponseMapper.toEntity(response);
   }
   
   @override
-  Future<ReserveOrderEntity> reserveBuyOrder() {
-    // TODO: implement reserveBuyOrder
-    throw UnimplementedError();
+  Future<void> reserveBuyOrder(ReserveOrderEntity body, String marketCode) async {
+    final ReserveOrderRequestDto request = ReserveOrderMapper.toDto(body);
+    return await _orderDataSource.reserveBuyOrder(request, marketCode);
   }
   
   @override
-  Future<ReserveOrderEntity> reserveSellOrder() {
-    // TODO: implement reserveSellOrder
-    throw UnimplementedError();
+  Future<void> reserveSellOrder(ReserveOrderEntity body, String marketCode) async {
+    final ReserveOrderRequestDto request = ReserveOrderMapper.toDto(body);
+    return await _orderDataSource.reserveBuyOrder(request, marketCode);
   }
   
 }
