0b13921e654dedb92bb2151e1c1963ca367768c8
parkyuhyeon <pyh1111802@naver.com>
2025-07-10 20:10:03 +0900

    :lipstick: :: Base Appbar 퍼블리싱

---
+++ lib/core/config/widget/base_appbar.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jusicool_design_system/jusicool_design_system.dart';
+
+class BaseAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const BaseAppbar({
+    super.key,
+    this.title = '',
+    this.centerTitle = true,
+    this.textStyle,
+    this.backgroundColor,
+    this.actionWidgets,
+    this.backButton = false,
+  });
+
+  final String title;
+  final bool centerTitle;
+  final TextStyle? textStyle;
+  final Color? backgroundColor;
+  final bool backButton;
+  final List<Widget>? actionWidgets;
+  final double elevation = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(title),
+      centerTitle: centerTitle,
+      backgroundColor: backgroundColor ?? JusicoolColor.white,
+      elevation: elevation,
+      scrolledUnderElevation: 0.0,
+      titleTextStyle: textStyle ?? JusicoolTypography.subTitle,
+      leading: IconButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        icon: JusicoolIcon.backArrow(
+          width: 24.w,
+          height: 24.h,
+          color: JusicoolColor.black,
+        ),
+      ),
+      actionsPadding: EdgeInsets.only(right: 24),
+      actions: actionWidgets ?? [],
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(57.h);
+}
