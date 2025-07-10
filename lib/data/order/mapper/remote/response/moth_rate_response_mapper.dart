fb607c962ea3417c9e2cdc73b09cfff2d6c65f74
aiden30015 <loolloo080313@gmail.com>
2025-07-10 09:59:16 +0900

    ✨ :: Month rate response mapper class

---
+++ lib/data/order/mapper/remote/response/moth_rate_response_mapper.dart
@@ -0,0 +1,26 @@
+import 'package:jusicool_ios/data/order/dto/remote/response/month_rate_response_dto.dart';
+import 'package:jusicool_ios/domain/order/entities/month_rate_entity.dart';
+class MothRateResponseMapper {
+  static MonthRateResponseDto toDto(MonthRateEntity entity) =>
+      MonthRateResponseDto(
+        monthlyRate: entity.monthlyRate,
+        markets: entity.markets.map((e) => MarketDto(
+          market: e.market,
+          koreanName: e.koreanName,
+          rate: e.rate,
+          proceed: e.proceed,
+          day: e.day,
+        )).toList(),
+      );
+  static MonthRateEntity toEntity(MonthRateResponseDto dto) =>
+      MonthRateEntity(
+        monthlyRate: dto.monthlyRate,
+        markets: dto.markets.map((e) => MarketEntity(
+          market: e.market,
+          koreanName: e.koreanName,
+          rate: e.rate,
+          proceed: e.proceed,
+          day: e.day,
+        )).toList(),
+      );
+}
