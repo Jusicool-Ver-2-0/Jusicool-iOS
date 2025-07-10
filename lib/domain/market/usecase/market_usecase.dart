df4592078eca6165e7d0805701a3c50f2d606007
aiden30015 <loolloo080313@gmail.com>
2025-07-10 15:55:40 +0900

    ✨ :: Market usecase class

---
+++ lib/domain/market/usecase/market_usecase.dart
@@ -0,0 +1,7 @@
+import 'package:jusicool_ios/domain/market/entities/market_entity.dart';
+
+abstract class MarketUsecase {
+  Future<List<MarketEntity>> fetchMarkets(String type);
+
+  Future<List<MarketEntity>> searchMarkets(String query);
+} 
